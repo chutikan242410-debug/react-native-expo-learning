2213eba9a8e7d8588c96f56a4a7ab5261bbcb69d
chutikan wutichai <chutikan242410@gmail.com>
2025-08-24 14:26:11 +0700

    feat: set up React Navigation with Home Details and Profile screens

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,4 +1,5 @@
 import { createStackNavigator } from "@react-navigation/stack";
+import LoginScreen from "../pages/LoginScreen";
 import HomeScreen from "../pages/HomeScreen";
 import DetailsScreen from "../pages/DetailsScreen";
 import ProfileScreen from "../pages/ProfileScreen"; 
@@ -9,20 +10,24 @@ import StyleExampleScreen from "../pages/StyleExampleScreen";
 import FlexboxExampleScreen from "../pages/FlexboxExampleScreen";
 import GridExampleScreen from "../pages/GridExampleScreen";
 
+
+
 const stack = createStackNavigator();
 
 export default function Navigation (){
   return (
       <stack.Navigator>
+        <stack.Screen name="Login" component={LoginScreen} />
         <stack.Screen name="Profile" component={ProfileScreen} />
-        <stack.Screen name="Menu" component={MenuScreen} />
         <stack.Screen name="Home" component={HomeScreen} />
+        <stack.Screen name="Menu" component={MenuScreen} />
         <stack.Screen name="Details" component={DetailsScreen} />
         <stack.Screen name="Counter" component={CounterScreen} />
         <stack.Screen name="ListMangerScreen" component={ListManagerScreen} />
         <stack.Screen name="StyleExampleScreen" component={StyleExampleScreen} />
         <stack.Screen name="FlexboxExampleScreen" component={FlexboxExampleScreen} />
         <stack.Screen name="GridExampleScreen" component={GridExampleScreen} />
+        
       </stack.Navigator>
   );
 }   
